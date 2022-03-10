6cc447d3d919dfcb9d23813064581c351f9aebd0
DianaBeki <dianabeki54@gmail.com>
2022-03-10 11:22:06 -0800

    hey

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * * main - Entry point
- * *
- * * puts -print declared char string to std output
- * *
- * * Return: Always 0 (Success)
- * */
+ *  * main - prints the phrase "Programming is like building a multilingual puzzle
+ *   * Return: 0 if exited properly, non-zero otherwise
+ *    */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
