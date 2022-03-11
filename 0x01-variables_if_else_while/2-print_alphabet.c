bc637651f41ee37e690aced39582b5caeac7c6ec
DianaBeki <dianabeki54@gmail.com>
2022-03-11 06:53:39 -0800

    me

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,17 +4,16 @@
  *  * main - Prints the alphabet.
  *   *
  *    * Return: Always 0 (Success)
- *
-     */
-/*
- *
- int main(void)
+ *     */
+
+int main(void)
 {
 		char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 			int i;
 
 				for (i = 0; i < 26; i++)
-		{			putchar(alp[i]);
+						{
+									putchar(alp[i]);
 										}
 					putchar('\n');
 						return (0);
