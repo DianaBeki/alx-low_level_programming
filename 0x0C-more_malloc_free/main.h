9d0d82e8cd769118d5f996710145b50837a53cbe
DianaBeki <dianabeki54@gmail.com>
2022-04-07 13:40:21 +0300

    two positive numbers

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,14 +1,22 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* _putchar - writes the character c to stdout */
 int _putchar(char c);
-void *malloc_checked(unsigned int b);
-char *string_nconcat(char *s1, char *s2, unsigned int n);
-void *_calloc(unsigned int nmemb, unsigned int size);
-int *array_range(int min, int max);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-void errors(void);
-int _strlen(char *s);
-int is_digit(char *s);
 
-#endif
+/* malloc_checked - allocates memory using malloc.*/
+void *malloc_checked(unsigned int b);
+
+/* string_nconcat -  concatenates two strings.*/
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+
+/* _calloc -  allocates memory for an array, using malloc.*/
+void *_calloc(unsigned int nmemb, unsigned int size);
+
+/* array_range - creates an array of integers.*/
+int *array_range(int min, int max);
+
+/* _realloc - reallocates a memory block using malloc and free.*/
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+#endif /* MAIN_H */
